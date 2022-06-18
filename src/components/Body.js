b571a67d6bd657ada145dac0ddfbc6d198cd2fa8
yamil schopp <yamilschopp@gmail.com>
2022-06-18 12:01:17 -0300

    commit Navbar

---
+++ src/components/Body.js
@@ -0,0 +1,15 @@
+import './Body.css';
+import Card from './Card'
+
+function Body() {
+  return (
+    <section className="cuerpo">
+        <Card />
+        <Card />
+        <Card />
+    </section>
+
+  );
+}
+
+export default Body;
---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import './Card.css';
+
+function Card() {
+  return (
+    <div className='card'>
+       <h4>Producto imagen</h4>
+       <p>Descripción</p>
+       <p>precio</p>
+    </div>
+  );
+}
+
+export default Card;
